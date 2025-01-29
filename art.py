6a03707abf9916c94c055fb30e7a84bbcc9aef4b
Sonia Tyburczy <styburczy.business@gmail.com>
2025-01-29 18:00:15 -0500

    Added error handling to test bug of glyph showing up undefined

--- art.py
+++ art.py
@@ -21,16 +21,11 @@ input_art = """
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 """
 
-
-"""
-This is pretty much a braille conversion program?
-Is it excessive...maybe
-Anyways, here we are
-"""
 import csv
 
 # Initializing our glyph dot dictionary
 braille_values = {}
+dot_braille_values = {}
 
 # Reading our glyph_storage file and storing it as a dictionary
 with open('glyph_storage.csv', newline='', encoding='utf-8') as csvfile:
@@ -42,15 +37,42 @@ with open('glyph_storage.csv', newline='', encoding='utf-8') as csvfile:
       numbers, glyph = column
       # Store glyph as key and dots as value
       braille_values[glyph] = numbers
+      # Opposite for dot_braille_values
+      dot_braille_values[numbers] = glyph
 
 
 def unglyph(glyph_string):
     number_list = []
 
     for glyph in glyph_string:
-        numbers = braille_values.get(glyph, 'other')
-        number_list.append(numbers)
-
+         # In case of it being a new line
+         if glyph == '\n':
+            number_list.append('\n')
+         # Otherwise just match and append
+         else:
+            numbers = braille_values.get(glyph, 'undefined')
+            if numbers == 'undefined':
+               print(f"Glyph '{glyph}' caused an undefined value.")
+            number_list.append(numbers)
+         
     return number_list
 
-new_dot = unglyph(input_art)
+def print_braille(number_list):
+   printable = ""
+
+   for number in number_list:
+      if number == '\n':
+         printable += '\n'
+      else:
+         glyph = dot_braille_values.get(number, '')
+         printable += str(glyph)
+
+   return printable
+    
+unglyphed_art = unglyph(input_art)
+glyphed_art = print_braille(unglyphed_art)
+print(glyphed_art)
+print("braille_values:", braille_values)
+print("dot_braille_values:", dot_braille_values)
+print("unglyphed_art:", unglyphed_art)
+print("glyphed_art:", glyphed_art)
